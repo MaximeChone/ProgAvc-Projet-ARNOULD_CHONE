cd09e504ec69a2d4cbc8fbde1aec34782af84b21
MaximeChone <31918084+MaximeChone@users.noreply.github.com>
2017-10-05 12:11:30 +0200

    Add files via upload

--- Towerdefense/TowerDefense.c
+++ Towerdefense/TowerDefense.c
@@ -2,3 +2,37 @@
 #include <math.h>
 #include "fonction.c"
 #define PI 3.14159265358979323846
+
+int main()
+{
+	cm **tab = malloc(sizeof(*tab) *17);
+	int i;
+	SDL_Surface *screen , *temp,*herbe;
+	SDL_Rect position;
+	position.x = 0;
+	position.y = 0;
+	for (i=0 ; i <17; i++)
+		tab[i] = malloc(sizeof(**tab) * 17);
+
+
+	SDL_Init(SDL_INIT_VIDEO);
+	SDL_WM_SetCaption("Tower Defense", "Tower Defense");
+	screen = SDL_SetVideoMode(816 ,816 , 0, 0);
+	temp  = SDL_LoadBMP("sprite_herbe.bmp");
+	herbe = SDL_DisplayFormat(temp);
+	SDL_FreeSurface(temp);
+	for (i=0 ; i<17 ; i++)
+		for (int j=0 ; j<17 ; j++)
+			{
+				position.x = i*48;
+				position.y = j*48;
+				SDL_BlitSurface(herbe, NULL, screen, &position);
+			}
+	SDL_UpdateRect(screen, 0, 0, 0, 0);
+	sleep(1);
+	free(*tab);
+	SDL_FreeSurface(screen);
+
+	return EXIT_SUCCESS;
+}
+	
--- Towerdefense/fonction.h
+++ Towerdefense/fonction.h
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+enum case_type {HERBE , MONTAGNE , CHEMIN_H , CHEMIN_V , CHEMIN_BG , CHEMIN_BD , CHEMIN_HG , CHEMIN_HD , DEBUT , FIN};
+
+typedef enum case_type case_type;
+
+struct tower
+{
+};
+
+typedef struct tower tour;
+
+struct case_map
+{
+	case_type type;       //type du sol
+	int occupe;           //si la case est occupé
+	tour tour;           //la tour sur la case
+};
+
+typedef struct case_map cm;
